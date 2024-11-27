3c1dcfc6b8f29dee852173882097fe31619ec6e3
dimasgusti <dimsgusticode@gmail.com>
2024-11-28 00:52:12 +0800

    feat: installing tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.{html,js}',  
+    './static/**/*.{html,js}',     
+    './**/*.html',                 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
