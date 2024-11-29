13a4cecfa2af456e70deab3518a500750d578208
dimasgusti <dimsgusticode@gmail.com>
2024-11-29 13:57:49 +0800

    bug: minimum product price & stock

--- products/forms.py
+++ products/forms.py
@@ -8,8 +8,8 @@ class ProductForm(forms.ModelForm):
         widgets = {
             'name': forms.TextInput(attrs={'class': 'form-control'}),
             'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
-            'price': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01'}),
-            'stock': forms.NumberInput(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control', 'step': '0.01', 'min': '1'}),
+            'stock': forms.NumberInput(attrs={'class': 'form-control', 'min': '1'}),
             'category': forms.Select(attrs={'class': 'form-control'}),
             'color': forms.Select(attrs={'class': 'form-control'}),
             'size': forms.Select(attrs={'class': 'form-control'}),
